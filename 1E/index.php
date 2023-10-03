f8e91c31592da55c5c17b7d51840984b95f86f98
Zackjw70 <zjwyeth@email.neit.edu>
2023-10-03 14:08:07 -0400

    1E Finished

---
+++ 1E/index.php
@@ -0,0 +1,12 @@
+<?php
+//task keys and values
+$task = [
+    'title' => 'Finish assignment',
+    'due' => '10/2/2023',
+    'assigned_to' => 'Zachary Wyeth',
+    'started' => true,
+    'completed' => true
+
+];
+//the require that has it use the view page
+require 'index.view.php';
